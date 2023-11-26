d2dd4cab14473825f791b0b4160bae9b08d97904
gianluca <alex992palermo@gmail.com>
2023-11-26 20:35:28 +0100

    Aggiunto Esercizio Polimorfismo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Forma rettangolo1 = new Rettangolo(7.5, 4.5);
+        rettangolo1.calcolaArea();
+        Forma triangolo1 = new Triangolo(3.5, 6);
+        triangolo1.calcolaArea();
+    }
+}
---
+++ src/Triangolo.java
@@ -0,0 +1,38 @@
+public class Triangolo extends Forma{
+    private double base;
+    private double altezza;
+    Triangolo(double base, double altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
+
+    @Override
+    public void calcolaArea() {
+        super.calcolaArea();
+        System.out.println(getBase() * getAltezza() /2);
+    }
+
+    @Override
+    public String toString() {
+        return "Triangolo{" +
+                "base=" + base +
+                ", altezza=" + altezza +
+                '}';
+    }
+}
